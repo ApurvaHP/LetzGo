b40d9068afdd2f20cdd20c2b747bcff6dbc7354a
Rudra <vidit.maniyar@gmail.com>
2015-04-27 17:20:55 -0700

    Added /logout api to user_auth.js file. Next update : Complete Session managed login logout functionality

--- LetzGo/routes/user_auth.js
+++ LetzGo/routes/user_auth.js
@@ -52,4 +52,16 @@ router.post('/login', function(req, res) {
 	});
 });
 
+
+/* For User Logout */
+router.get('/logout', function(req, res) {
+	req.session.destroy(function(err) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.redirect('/');
+		}
+	});
+});
+
 module.exports = router;
